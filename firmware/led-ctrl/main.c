626a51f5dea7dadf791b66c0e8a2001fd9c28f03
Marco Rossini <marco.rossini@phys.ethz.ch>
2014-12-16 16:31:47 +0100

    Firmware: Changed PWM mode to phase correct such that 0 % and 100 % duty cycle are possible.

--- firmware/led-ctrl/main.c
+++ firmware/led-ctrl/main.c
@@ -4,7 +4,7 @@
 #include "lookup.h"
 
 /* Lookup table that converts a linear range to an exponential function from 1 up to 8192 in 15 steps */
-int16_t intensity_lookup [] = {1, 2, 3, 6, 11, 20, 37, 67, 122, 223, 406, 741, 1351, 2464, 4493, 8192};
+int16_t intensity_lookup [] = {1, 2, 3, 5, 9, 16, 28, 49, 84, 147, 256, 446, 776, 1351, 2353, 4096};
 
 int main (void) {
 	/* Set clock to 4 MHz by setting the clock division (prescale) to 2 */
@@ -26,24 +26,24 @@ int main (void) {
 	DDRD = 0xff;
 
 
-	// initialise 16-bit timer
-	// turn off PRM
+	// Initialise 16-bit timer
+	// Turn off Power Reduction Mode
 	PRR &= ~(1<<PRTIM1);
-	// set as output
+	// Set OC1A pin as output
 	DDRD |= (1<<PD5);
-	// set output compare pin to non-inverting PWM
+	// Set OC1A output compare pin to clear on upcount and set at downcount
 	TCCR1A |= (1<<COM1A1);
 	TCCR1A &= ~(1<<COM1A0);
-	// Fast PWM
+	// Set mode to phase correct PWM with ICR as TOP
+	TCCR1B |= (1<<WGM13);
+	TCCR1B &= ~(1<<WGM12);
 	TCCR1A |= (1<<WGM11);
 	TCCR1A &= ~(1<<WGM10);
-	TCCR1B |= (1<<WGM12);
-	TCCR1B |= (1<<WGM13);
-	// define TOP value
-	ICR1 = 8192;
-	// define duty cycle
-	OCR1A = 2048;
-	// set no prescale and start
+	// Define TOP value (frequency = f_io / (2 * N * TOP), N = prescale factor)
+	ICR1 = 4096;
+	// Set the duty cycle initially to 0/4096
+	OCR1A = 0;
+	// Set no clock prescale and start
 	TCCR1B |= (1<<CS10);
 
 	// setup ADC
@@ -66,7 +66,11 @@ int main (void) {
 		while(ADCSRA & (1<<ADSC));
 
 		/* Calculate intensity from ADC reading */
-		uint16_t intensity = lookup(ADC, intensity_lookup, 32, 992, 6);
+		uint16_t intensity;
+		if (ADC < 16)
+			intensity = 0;
+		else
+			intensity = lookup(ADC, intensity_lookup, 32, 992, 6);
 
 		/* Set PWM duty cycle */
 		OCR1A = intensity;
